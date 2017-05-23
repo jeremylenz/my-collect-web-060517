fbadbd23385e1f1ca2cba2eeae32768530f3d15c
Jeremy Lenz <jeremylenz@gmail.com>
2017-05-23 19:15:47 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
+def my_collect(my_array)
 
-
+  my_new_array = Array.new(my_array)
+# binding.pry
+  i = 0
+  while i < my_new_array.length
+    my_new_array[i] = yield(my_new_array[i])
+    i += 1
+  end
+# binding.pry
+  my_new_array
+  end
